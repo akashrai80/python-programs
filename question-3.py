b3a6180e977a80571e444c3161c27f1e804f7b1f
Akash Rai <69968941+akashrai80@users.noreply.github.com>
2021-06-03 18:14:32 +0530

    Update question-3.py

--- question-3.py
+++ question-3.py
@@ -1,6 +1,4 @@
 import json
 string = {'4': 5, '6': 7, '1': 3, '2': 4}
-print("String:")
-print(string)
-print("\nJSON data:")
-print(json.dumps(string, sort_keys=True, indent=4))
+print("String: {}".format(string))
+print("\nJSON data: {}".format(json.dumps(string, sort_keys=True, indent=4)))
